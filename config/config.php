266a24ab80def3c3a94b426e7a6b95bb8a099bbc
DavidProyectos0804 <davidproyectos0804@gmail.com>
2026-07-02 15:01:24 +0200

    Empezando a desplegar la aplicacion

--- config/config.php
+++ config/config.php
@@ -1,7 +1,13 @@
 <?php
-    define("SERVIDOR","localhost");
-    define("USUARIO","root");
-    define("PASSWORD",'');
-    define("BBDD","gestion_genshin");
-    #http://localhost/index.php?controlador=auth&accion=loginForm
-?>
+if ($_SERVER['SERVER_NAME'] === 'localhost') {
+    define("SERVIDOR", "localhost");
+    define("USUARIO", "root");
+    define("PASSWORD", '');
+    define("BBDD", "gestion_genshin");
+} else {
+    define("SERVIDOR", "sql201.infinityfree.com");
+    define("USUARIO", "if0_42320678");
+    define("PASSWORD", "tu_password");
+    define("BBDD", "if0_42320678_gestiongenshin");
+}
+#http://localhost/index.php?controlador=auth&accion=loginForm   
